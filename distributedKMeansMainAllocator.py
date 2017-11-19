83028bc62e98f4c6cd3c57812db6f8456f569ea8
Arjun Ahuja <cs14btech11004@iith.ac.in>
2017-11-19 13:18:39 +0530

    adding distributed solutions to kmeans with master allocation system and adding the mapping between image and number of clusters

---
+++ distributedKMeansMainAllocator.py
@@ -0,0 +1,102 @@
+'''
+Idea create a queue of requests and assign each node one request as and when it happens
+Assuming each image on each of the requesters server
+'''
+
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import argparse
+import utils
+import cv2
+import random
+import numpy as np
+from sklearn.metrics import silhouette_samples, silhouette_score
+import matplotlib.image as mpimg
+import os
+import math
+
+def load_images(folder):
+    images = []
+    imageFileNames = []
+    for filename in os.listdir(folder):
+        img = mpimg.imread(os.path.join(folder, filename))
+        if img is not None:
+            imageFileNames.append(filename)
+    return imageFileNames
+
+imageFileNames = load_images("./database/")
+
+fil = open("DistributedDatabase.txt","w")
+fil_existing_database = open("database.txt","r")
+
+'''
+Array to store for which files kmeans has already happend
+'''
+
+fileNameDict = []
+
+for line in fil_existing_database:
+    fileNameDict.append(line.split(' ')[0])
+
+'''
+introduce a new thread here for server and listerner
+'''
+
+notYetKmeaned = []
+for fileName in imageFileNames:
+    if fileName not in fileNameDict:
+        notYetKmeaned.append(fileName)
+
+'''
+Listener And Allocator
+NOTE: Assumption No Failures
+'''
+
+'''
+    Simple socket server using threads
+'''
+
+import socket
+import sys
+
+HOST = ''
+PORT = 8889
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print 'Socket created'
+
+try:
+    s.bind((HOST, PORT))
+except socket.error as msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+
+print 'Socket bind complete'
+
+#Start listening on socket
+s.listen(10)
+print 'Socket now listening'
+
+intialLengthOfNotYetKmeaned = len(notYetKmeaned)
+
+recieved = 0
+
+while(len(notYetKmeaned)>0 or recieved<intialLengthOfNotYetKmeaned):
+    #wait to accept a connection - blocking call
+    conn, addr = s.accept()
+    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+    data = conn.recv(1024)
+    print data
+    if(len(data) == 1):
+        #request recieved
+        toAllocate = "no"
+        if(len(notYetKmeaned) != 0):
+            toAllocate = notYetKmeaned.pop()
+        conn.sendall(toAllocate)
+    else:
+        '''
+        If Answer comes write to file
+        '''
+        recieved+=1
+        print 'response recieved: ',data
+        fil.write(data+'\n')
---
+++ distributedRequester.py
@@ -0,0 +1,68 @@
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import argparse
+import utils
+import cv2
+import random
+import numpy as np
+from sklearn.metrics import silhouette_samples, silhouette_score
+import matplotlib.image as mpimg
+import os
+import math
+
+import socket
+
+while(1):
+    '''
+        send a request to Allocator so that he/she may allocate node
+    '''
+    # create an ipv4 (AF_INET) socket object using the tcp protocol (SOCK_STREAM)
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(('', 8889))
+    client.send('0')
+    response = client.recv(4096)
+    if response == 'no':
+        #no request is left
+        print 'no request is left exiting!'
+        client.close()
+        break
+    '''
+        Now execute and find the best n for the kmeans
+    '''
+    #query image
+    image = cv2.imread("./database/"+response)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = image.reshape((image.shape[0] * image.shape[1], 3))
+
+    '''
+        Using Silhoutte score to get the best number of clusters
+        can make this distributed for each image can return the best n_cluster!
+    '''
+    n_cluster = 8
+    Prevscore = 2
+    count = 0
+    ElbowPoint = -1
+    for centers in range(2,50):
+        clt = KMeans(n_clusters = centers)
+        labelsP = clt.fit_predict(image)
+        silScore = silhouette_score(image,labelsP)
+        if Prevscore - silScore < .018:
+            count+=1
+            if count == 1:
+                ElbowPoint = centers - 1
+            if count > 3:
+                n_cluster = ElbowPoint
+                break
+        else:
+            count = 0
+        n_cluster = centers
+        Prevscore = silScore
+    '''
+        Send the best n back to the Allocator
+    '''
+    print 'response sent: ',response+" "+str(n_cluster)+"\n"
+    client.close()
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(('', 8889))
+    client.sendall(response+" "+str(n_cluster)+"\n")
+    client.close()
